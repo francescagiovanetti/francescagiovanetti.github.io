1a59d3c9eac5e04fe2087305a97235640823a3d1
francescagiovanetti <giovanettifrancesca98@gmail.com>
2026-03-24 08:59:09 +0000

    Deploying to gh-pages from @ francescagiovanetti/francescagiovanetti.github.io@ba76b4c9ff6e11acc80853373b4923d885a6da86 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,13 +3,20 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-",
+    title: "",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
+  },{id: "nav-",
+          title: "",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/";
+          },
+        },{id: "nav-blog",
           title: "blog",
           description: "",
           section: "Navigation",
